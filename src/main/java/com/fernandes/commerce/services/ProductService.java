699ba696d07df4759a1d32f66e3f476287c10c8f
root <root@Lucas>
2023-08-02 12:04:07 -0300

    GET products page

--- src/main/java/com/fernandes/commerce/services/ProductService.java
+++ src/main/java/com/fernandes/commerce/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.fernandes.commerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,12 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
     
 }
